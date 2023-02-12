d2ac90c9e613093f549a613e25089ddb547a4d37
erzh <erzh@umich.edu>
2023-02-12 12:33:05 -0500

    Configured aimbot for the court

--- 497/Assets/Scripts/BallHandler.cs
+++ 497/Assets/Scripts/BallHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BallHandler : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class BallHandler : MonoBehaviour
     bool hit = false;
     float lastSwing;
     public GameObject ball;
+    public float force = 12;
     private Rigidbody ballPhysics;
 
     void Start()
@@ -44,17 +46,14 @@ public class BallHandler : MonoBehaviour
     {
         Vector3 currVelocity = ballPhysics.velocity;
         Vector3 normalVector = transform.rotation * Vector3.right; // will probably have to change later
-        //Debug.Log(normalVector);
+        // Bounce the ball off the racket first
         ballPhysics.velocity = Vector3.Reflect(ballPhysics.velocity, normalVector);
         ballPhysics.velocity *= 0.8f;
-
-        float force = 12;
+        
+        Vector3 aim;
+        /*
         int aimPos = 0;
         float height = 1;
-        Vector3 aim;
-        //straight force
-        //aim = new Vector3(10,0,0);
-
         switch (aimPos)
         {
             case -1:
@@ -81,7 +80,58 @@ public class BallHandler : MonoBehaviour
         }   
         ballPhysics.AddForce(aim*force, ForceMode.VelocityChange);
         
-        ballPhysics.AddForce(0, height*4 + 4,0, ForceMode.VelocityChange);
+        ballPhysics.AddForce(0, height*4 + 4,0, ForceMode.VelocityChange);*/
+
+        // then apply aimBot force
+        aim = aimBot();
+        ballPhysics.AddForce(aim, ForceMode.VelocityChange);
+    }
+    private Vector3 aimBot()
+    {
+        Transform otherPlayer = gameObject.GetComponentInParent<Transform>();
+        Vector3 otherPos = otherPlayer.position;
+        // aimbot toward center is default
+        Vector3 aim = Vector3.Normalize(Vector3.MoveTowards(-ballPhysics.position, Vector3.zero, 1));
+        aim = Vector3.ProjectOnPlane(aim, Vector3.up);
+        if (Math.Abs(gameObject.GetComponentInParent<Transform>().position.x) > 6) // not too close to aim
+        {
+            if (otherPos.z <= -2) // opponent is far right
+            {
+                Debug.Log("Mid Left");
+                // aim toward the mid left
+                aim = Vector3.Normalize(new Vector3(-ballPhysics.position.x, 0, -ballPhysics.position.z + 1.5f));
+            }
+            else if (otherPos.z >= 2) // opponent is far left
+            {
+                Debug.Log("Mid Right");
+                // aim toward the mid right
+                aim = Vector3.Normalize(new Vector3(-ballPhysics.position.x, 0, -ballPhysics.position.z - 1.5f));
+            }
+            else if (otherPos.z < 0) // oponent is mid right
+            {
+                Debug.Log("Far Left");
+                // aim toward the far left
+                aim = Vector3.Normalize(new Vector3(-ballPhysics.position.x, 0, -ballPhysics.position.z + 3));
+            }
+            else if (otherPos.z > 0)
+            {
+                Debug.Log("Far Right");
+                // aim toward the far right
+                aim = Vector3.Normalize(new Vector3(-ballPhysics.position.x, 0, -ballPhysics.position.z - 3));
+            }
+        }
+        aim *= force;
+        aim += Vector3.up * 3;
+        if (Math.Abs(gameObject.GetComponentInParent<Transform>().position.x) > 4) 
+        {
+            aim += Vector3.up * 3;
+            if (force < 10)
+            {
+                aim += Vector3.up * 2;
+            }
+        }
+        
+        return aim;
     }
     public bool getSwing()
     {
