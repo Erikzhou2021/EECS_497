3019b91a728514ba3b8e451046099c764d7d2893
Sonia Lin <sonialin@umich.edu>
2023-02-28 10:08:08 -0500

    keyboard movement

--- 497/Assets/Scripts/Keyboardmovement.cs
+++ 497/Assets/Scripts/Keyboardmovement.cs
@@ -24,117 +24,56 @@ namespace Mirror
         // Update is called once per frame
         void FixedUpdate()
         {
-            if (resetCounter > 0)
+            if (isLocalPlayer)
             {
-                resetCounter++;
-            }
-            if (resetCounter > 5)
-            {
-                // lmao dont look, resetting all variables 
-                BallBoundary.Instance.touchedGroundOnceOut = false;
-                BallBoundary.Instance.scoreStop = false;
-                BallBoundary.Instance.bouncedInOpponentCourtOnce = false;
-                BallBoundary.Instance.playerTurn = 0;
-                ballPhysics.GetComponent<Rigidbody>().useGravity = true;
-                bh.Serve();
-                resetCounter = 0;
-            }
-            if (Input.GetKey("w") || Input.GetKey("up"))
-            {
-                //Debug.Log("Hi");
-                gameObject.transform.Translate(playerSpeed, 0, 0, Space.World);
-            }
-            if (Input.GetKey("a") || Input.GetKey("left"))
-            {
-                gameObject.transform.Translate(0, 0, playerSpeed, Space.World);
-            }
-            if (Input.GetKey("s") || Input.GetKey("down"))
-            {
-                gameObject.transform.Translate(-playerSpeed, 0, 0, Space.World);
-            }
-            if (Input.GetKey("d") || Input.GetKey("right"))
-            {
-                gameObject.transform.Translate(0, 0, -playerSpeed, Space.World);
-            }
-            if (Input.GetKey("r"))
-            {
-                resetCounter = 1; // just delaying the serve for a few frames so the other code can run
-                GameManager.Instance.state = GameState.Serve;
-            }
-            if (Input.GetKey("q"))
-            {
-                gameObject.transform.Rotate(0, -0.5f, 0);
-            }
-            if (Input.GetKey("e"))
-            {
-                resetCounter = Time.time;
-                ballPhysics = ball.GetComponent<Rigidbody>();
-                bh = racket.GetComponent<BallHandler>();
-            }
-        }
-
-            // Update is called once per frame
-            void FixedUpdate()
-            {
-                if (isLocalPlayer)
+                if (resetCounter > 0)
                 {
-                    if (resetCounter > 0)
-                    {
-                        resetCounter++;
-                    }
-                    if (resetCounter > 5)
-                    {
-                        // lmao dont look, resetting all variables 
-                        BallBoundary.Instance.touchedGroundOnceOut = false;
-                        BallBoundary.Instance.scoreStop = false;
-                        BallBoundary.Instance.bouncedInOpponentCourtOnce = false;
-                        BallBoundary.Instance.playerTurn = 0;
-                        ballPhysics.GetComponent<Rigidbody>().useGravity = true;
-                        bh.Serve();
-                        resetCounter = 0;
-                    }
-                    if (Input.GetKey("w") || Input.GetKey("up"))
-                    {
-                        Debug.Log("Hi");
-                        gameObject.transform.Translate(playerSpeed, 0, 0);
-                    }
-                    if (Input.GetKey("a") || Input.GetKey("left"))
-                    {
-                        gameObject.transform.Translate(0, 0, playerSpeed);
-                    }
-                    if (Input.GetKey("s") || Input.GetKey("down"))
-                    {
-                        gameObject.transform.Translate(-playerSpeed, 0, 0);
-                    }
-                    if (Input.GetKey("d") || Input.GetKey("right"))
-                    {
-                        gameObject.transform.Translate(0, 0, -playerSpeed);
-                    }
-                    if (Input.GetKey("r"))
-                    {
-                        resetCounter = 1; // just delaying the serve for a few frames so the other code can run
-                        GameManager.Instance.state = GameState.Serve;
-                    }
-                    if (Input.GetKey("q"))
-                    {
-                        gameObject.transform.Rotate(0, -0.5f, 0);
-                    }
-                    if (Input.GetKey("e"))
-                    {
-                        gameObject.transform.Rotate(0, 0.5f, 0);
-                    }
-                    if (Input.GetKey("f"))
-                    {
-                        Vector3 force = Vector3.MoveTowards(ballPhysics.position, racket.transform.position, 1);
-                        force.Normalize();
-                        force = force * 10;
-                        ballPhysics.AddForce(force);
-                    }
-                    if (Input.GetMouseButton(0) && !bh.GetSwing())
-                    {
-                        bh.StartSwing(8);
-                    }
+                    resetCounter++;
+                }
+                if (resetCounter > 5)
+                {
+                    // lmao dont look, resetting all variables 
+                    BallBoundary.Instance.touchedGroundOnceOut = false;
+                    BallBoundary.Instance.scoreStop = false;
+                    BallBoundary.Instance.bouncedInOpponentCourtOnce = false;
+                    BallBoundary.Instance.playerTurn = 0;
+                    ballPhysics.GetComponent<Rigidbody>().useGravity = true;
+                    bh.Serve();
+                    resetCounter = 0;
+                }
+                if (Input.GetKey("w") || Input.GetKey("up"))
+                {
+                    //Debug.Log("Hi");
+                    gameObject.transform.Translate(playerSpeed, 0, 0, Space.World);
+                }
+                if (Input.GetKey("a") || Input.GetKey("left"))
+                {
+                    gameObject.transform.Translate(0, 0, playerSpeed, Space.World);
+                }
+                if (Input.GetKey("s") || Input.GetKey("down"))
+                {
+                    gameObject.transform.Translate(-playerSpeed, 0, 0, Space.World);
+                }
+                if (Input.GetKey("d") || Input.GetKey("right"))
+                {
+                    gameObject.transform.Translate(0, 0, -playerSpeed, Space.World);
+                }
+                if (Input.GetKey("r"))
+                {
+                    resetCounter = 1; // just delaying the serve for a few frames so the other code can run
+                    GameManager.Instance.state = GameState.Serve;
+                }
+                if (Input.GetKey("q"))
+                {
+                    gameObject.transform.Rotate(0, -0.5f, 0);
+                }
+                if (Input.GetKey("e"))
+                {
+                    resetCounter = Time.time;
+                    ballPhysics = ball.GetComponent<Rigidbody>();
+                    bh = racket.GetComponent<BallHandler>();
                 }
             }
+        }
     }
 }
