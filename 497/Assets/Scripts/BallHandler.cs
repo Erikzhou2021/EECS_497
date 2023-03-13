ec4ca02d973c86c93bfe4730217687902ecbe248
Sonia Lin <sonialin@umich.edu>
2023-03-12 23:58:00 -0400

    rotate back frame not early

--- 497/Assets/Scripts/BallHandler.cs
+++ 497/Assets/Scripts/BallHandler.cs
@@ -77,12 +77,13 @@ public class BallHandler : MonoBehaviour
         GetComponent<TrailRenderer>().emitting = false;
         yield return new WaitForEndOfFrame();
 
-        while (transform.parent.position.x < 0 && (transform.localPosition.z > (-1.4))
-            || (transform.parent.position.x > 0 && (transform.localPosition.z < (1.4)))) //for forehand 
+        while (transform.parent.position.x < 0 && (transform.localPosition.z > (-1.49))
+            || (transform.parent.position.x > 0 && (transform.localPosition.z < (1.49)))) //for forehand 
         {
             transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
             yield return null;
         }
+        transform.localPosition = new Vector3(0, 0, 1.5f * Mathf.Sign(transform.parent.position.x));
     }
     void WindUp()
     {
