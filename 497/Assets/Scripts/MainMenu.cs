d37a2ecdf2823c64a0dfe15923f6477184eb15f7
chairiefaris <chairie66@gmail.com>
2023-03-16 10:02:52 -0400

    Host will now only start server

--- 497/Assets/Scripts/MainMenu.cs
+++ 497/Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void HostLobby()
     {
-        networkManager.StartHost();
+        networkManager.StartServer();
 
         landingPagePanel.SetActive(false);
     }
