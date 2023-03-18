ec102cd7ae01fe8916c68ff9516076de81fe0800
Sonia Lin <sonialin@umich.edu>
2023-03-18 15:03:30 -0400

    server tag fix i think

--- 497/Assets/Scripts/Extra/Bouncing.cs
+++ 497/Assets/Scripts/Extra/Bouncing.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Bouncing : MonoBehaviour
 {
     public float bounceSpeed = 5f;
-    public float bounceHeight = 0.01f; 
+    public float bounceHeight = 0.01f;
 
+    private float startYPos;
+    private void Awake()
+    {
+        startYPos = transform.position.y;
+    }
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y + (Mathf.Sin(Time.time * bounceSpeed) * bounceHeight), transform.position.z);
+        transform.position = new Vector3(transform.position.x, startYPos + (Mathf.Sin(Time.time * bounceSpeed) * bounceHeight), transform.position.z);
     }
 }
